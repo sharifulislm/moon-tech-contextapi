8f2a1ea3e1e0938a571f76005421c400e26d98c0
sharifulislm <shariful.islam.coder@gmail.com>
2022-12-24 19:00:30 +0300

    aded new server site

--- moon-tech-usereducer-contextapi/src/App.js
+++ moon-tech-usereducer-contextapi/src/App.js
@@ -1,26 +1,13 @@
-import { createContext, useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
+import ProductProvider from "./context/ProductProvider";
 import routes from "./routes/routes";
 
-const PRODUCT_CONTEXT = createContext();
 function App() {
- const[data,setData]=useState([]);
-
-useEffect(()=> {
-  fetch("products.json")
-  .then((res) => res.json())
-  .then((data)=> console.log(data))
-},[])
-const value ={
-  data,
-}
   return (
     <div>
-  
-      <PRODUCT_CONTEXT.Provider value={value}>
-      <RouterProvider router={routes} />
-      </PRODUCT_CONTEXT.Provider>
-
+      <ProductProvider>
+        <RouterProvider router={routes} />
+      </ProductProvider>
     </div>
   );
 }
