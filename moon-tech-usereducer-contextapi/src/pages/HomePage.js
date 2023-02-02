9ae8f4de4d2047eacb147fa595e0769bd29bd7bc
sharifulislm <shariful.islam.coder@gmail.com>
2023-02-02 22:52:50 +0300

    updated somethign

---
+++ moon-tech-usereducer-contextapi/src/pages/HomePage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className='m-'>
+            <h1> Home page copy </h1>
+
+            <section class="mb3">
+        <button id="ember278" class="artdeco-button artdeco-button--muted artdeco-button--3 artdeco-button--tertiary ember-view" type="button">  <li-icon aria-hidden="true" type="arrow-left" class="artdeco-button__icon" size="small"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="mercado-match" data-supported-dps="16x16" fill="currentColor" width="16" height="16" focusable="false">
+  <path d="M7 3L4.16 7H14v2H4.16L7 13H4.55L1 8l3.55-5z"></path>
+</svg></li-icon>
+
+<span class="artdeco-button__text">
+    Skill assessments
+</span></button>
+      </section>
+        </div>
+    );
+};
+
+export default HomePage;
